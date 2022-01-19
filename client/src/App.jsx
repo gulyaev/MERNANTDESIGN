193d50943c34662d95339fdda1f8305add675da3
gulyaev <gulyaev.gvs@gmail.com>
2022-01-19 21:24:54 +0500

    antdesign installed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import Header from './components/Header';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Registration from "../src/components/registration/Registration";
+import Grid from "../src/components/Grid";
+
 
 function App() {
   return (
-    <div>
-      AntDesign
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Header />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/grid" element={<Grid />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import "./header.css";
+import { PageHeader, Button, Typography, Row } from 'antd';
+const { Paragraph } = Typography;
+
+const routes = [
+    {
+        path: 'index',
+        breadcrumbName: 'First-level Menu',
+    },
+    {
+        path: 'first',
+        breadcrumbName: 'Second-level Menu',
+    },
+    {
+        path: 'second',
+        breadcrumbName: 'Third-level Menu',
+    },
+];
+
+const IconLink = ({ src, text }) => (
+    <a className="example-link">
+        <img className="example-link-icon" src={src} alt={text} />
+        {text}
+    </a>
+);
+
+const content = (
+    <>
+        <Paragraph>
+            Ant Design interprets the color system into two levels: a system-level color system and a
+            product-level color system.
+        </Paragraph>
+        <Paragraph>
+            Ant Design&#x27;s design team preferred to design with the HSB color model, which makes it
+            easier for designers to have a clear psychological expectation of color when adjusting colors,
+            as well as facilitate communication in teams.
+        </Paragraph>
+        <div>
+            <IconLink
+                src="https://gw.alipayobjects.com/zos/rmsportal/MjEImQtenlyueSmVEfUD.svg"
+                text="Quick Start"
+            />
+            <IconLink
+                src="https://gw.alipayobjects.com/zos/rmsportal/NbuDUAuBlIApFuDvWiND.svg"
+                text=" Product Info"
+            />
+            <IconLink
+                src="https://gw.alipayobjects.com/zos/rmsportal/ohOEPSYdDTNnyMbGuyLb.svg"
+                text="Product Doc"
+            />
+        </div>
+    </>
+);
+
+const Content = ({ children, extraContent }) => (
+    <Row>
+        <div style={{ flex: 1 }}>{children}</div>
+        <div className="image">{extraContent}</div>
+    </Row>
+);
+
+const Header = () => {
+    return (
+        <PageHeader
+            onBack={() => null}
+            title="Title"
+            subTitle="This is a subtitle"
+            extra={[
+                <Button key="2">Войти</Button>,
+                <Button key="1" type="primary">
+                    Регистрация
+                </Button>,
+            ]}
+            avatar={{ src: 'https://avatars1.githubusercontent.com/u/8186664?s=460&v=4' }}
+            breadcrumb={{ routes }}
+        >
+            <Content
+                extraContent={
+                    <img
+                        src="https://gw.alipayobjects.com/zos/antfincdn/K%24NnlsB%26hz/pageHeader.svg"
+                        alt="content"
+                        width="100%"
+                    />
+                }
+            >
+                {content}
+            </Content>
+        </PageHeader>
+    )
+};
+
+export default Header;
