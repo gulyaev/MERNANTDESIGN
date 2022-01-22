cdd2a167233a551f740cad02102d6a51c149c1d5
gulyaev <gulyaev.gvs@gmail.com>
2022-01-22 13:02:25 +0500

    Directories creation and popup

--- client/src/actions/file.js
+++ client/src/actions/file.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { setFiles } from "../reducers/fileReducer";
+import { setFiles, addFile } from "../reducers/fileReducer";
 
 export const getFiles = (dirId) => {
     return async dispatch => {
@@ -9,6 +9,23 @@ export const getFiles = (dirId) => {
             });
             dispatch(setFiles(response.data.files));
             console.log(response.data);
+        } catch (error) {
+            alert(error.response.data.message);
+        }
+    }
+}
+
+export const createDir = (dirId, name) => {
+    return async dispatch => {
+        try {
+            const response = await axios.post(`http://localhost:5000/api/files`, {
+                name,
+                parent: dirId,
+                type: 'dir'
+            }, {
+                headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}
+            });
+            dispatch(addFile(response.data));
         } catch (error) {
             alert(error.response.data.message);
         }
--- client/src/components/disk/Disk.jsx
+++ client/src/components/disk/Disk.jsx
@@ -1,19 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Row, Col } from 'antd';
 import { useDispatch, useSelector } from "react-redux";
 import { getFiles } from "../../actions/file";
 import "./disk.less";
 import FileList from "./fileList/FileList";
+import Popup from "./Popup";
 
 const Disk = () => {
     const dispatch = useDispatch();
     const currentDir = useSelector(state => state.files.currentDir);
+    const { size } = 'default';
+    const [isModalVisible, setIsModalVisible] = useState(false);
 
     useEffect(() => {
         dispatch(getFiles(currentDir))
     }, [currentDir]);
 
-    const { size } = 'default';
+    const showModal = () => {
+        setIsModalVisible(true);
+    }
 
     return (
         <div className="disk">
@@ -21,11 +26,12 @@ const Disk = () => {
                 <Col lg={6}>
                     <div className="disk__btns">
                         <Button size={size}>Назад</Button>
-                        <Button type="dashed" size={size} className="disk__create">Создать папку</Button>
+                        <Button type="dashed" size={size} className="disk__create" onClick = {() => showModal()}>Создать папку</Button>
                     </div>
                 </Col>
             </Row>
             <FileList />
+            <Popup isModalVisible={isModalVisible} setIsModalVisible={setIsModalVisible}/>
         </div>
     )
 }
